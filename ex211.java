34f79169ce7a9f205f62f1e655dec677209766f5
Marina <marinaesp08@gmail.com>
2026-02-03 18:56:31 +0100

    #9 - data types and initialization

---
+++ ex211.java
@@ -0,0 +1,16 @@
+class ex211 {
+/*
+This is a code example of variables
+*/
+    public static void main (String args[]) {
+
+        byte bagSize = 12; // from -128 to 127
+        char roast = 'M'; //a single character or letter in ''
+        int beans = 360;
+        double price = 8.99;
+        short qtySold = 3000;
+        long annualQtySold = 36000l; // ends with l to differ from int
+        float costPerOz = .49f; // ends with f to differ from double
+        boolean caffeine = true; // true or false
+    }
+}
