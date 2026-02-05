1f836e8ddce6f898adbb1b7b660ede6c8cb10bca
Marina <marinaesp08@gmail.com>
2026-02-05 12:16:38 +0100

    S3: 18. Procesamiento de datos en Java - Casting

--- EjercicioAtribuicao.java
+++ EjercicioAtribuicao.java
@@ -1,23 +1,25 @@
 public class EjercicioAtribuicao {
     public static void main (String [] args) {
-
-        /*el comando de atribuición implica que una variable (b, area) recibe (=)
-        una expresión (6.0) o el resultado de la operación y por tanto es dicho resultado
-        lo que se guarda en esa variable (area recibe 35.0)*/
-        double b, B, h, area;
-        b = 6.0;
-        B = 8.0;
-        h = 5.0;
-        area = (b + B) / 2.0 * h;
-
         /*
-        float b, B, h, area;
-        b = 6f;
-        B = 8f;
-        h = 5f;
-        area = (b + B) / 2f * h; 
+        int a, b;
+        double resultado;
+        a = 5;
+        b = 2;
+        resultado = a / b;
+
+        System.out.println(resultado);
+
+        // El resultado recibe 2.0 porque las variables a y b son números enteros. MAL. 
         */
 
-        System.out.println(area);
+        int a, b;
+        double resultado;
+        a = 5;
+        b = 2;
+        resultado = (double) a / b;
+
+        System.out.println(resultado);
+
+        //Hacemos casting (convertir un tipo de dato en otro) al poner double entre paréntesis para que el resultado sea decimal 2.5. BIEN.
     }
 }
