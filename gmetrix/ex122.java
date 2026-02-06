0ec33f44063f24608739322483e0354976e051a4
Marina <marinaesp08@gmail.com>
2026-02-06 13:58:01 +0100

    Creación carpeta gmetrix y traslado de archivos del curso anterior

--- gmetrix/ex122.java
+++ gmetrix/ex122.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 class ex122 {
     public static void main (String args[]) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Thank you for visiting us. What is your name?");
-        String name = in.nextLine();
+        String name = sc.nextLine();
         System.out.println("Welcome, " + name);
+        sc.close();
     }
 }
