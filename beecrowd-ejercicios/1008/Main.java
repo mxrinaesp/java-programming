d93df6ed070fe1e2992cc479bff6eadbdfdbb2e1
Marina <marinaesp08@gmail.com>
2026-02-06 21:25:28 +0100

    S3: 25. Ejercicios en beecrowd(1008)

---
+++ beecrowd-ejercicios/1008/Main.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int numFuncionario, qtdHoras, NUMBER;
+        double valorHoras, SALARY; 
+        numFuncionario = sc.nextInt();
+        qtdHoras = sc.nextInt();
+        sc.nextLine();
+        valorHoras = sc.nextDouble();
+        NUMBER = numFuncionario;
+        SALARY = qtdHoras * (double) valorHoras;
+        System.out.println("NUMBER = " + NUMBER);
+        System.out.printf("SALARY = U$ %.2f%n", SALARY);
+
+        sc.close();
+    }
+}
