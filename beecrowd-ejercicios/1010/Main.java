d4d2cd74a04736db5bf75af60ef871a46616c76d
Marina <marinaesp08@gmail.com>
2026-02-09 13:15:44 +0100

    S3: 25. Ejercicios en beecrowd(1010) - fixed

--- beecrowd-ejercicios/1010/Main.java
+++ beecrowd-ejercicios/1010/Main.java
@@ -6,17 +6,12 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int codigo1, qtd1, codigo2, qtd2;
+        int qtd1, qtd2;
         double valor1, valor2, valorTotal;
 
-        codigo1 = sc.nextInt();
         qtd1 = sc.nextInt();
-        sc.nextLine();
         valor1 = sc.nextDouble();
-        sc.nextLine();
-        codigo2 = sc.nextInt();
         qtd2 = sc.nextInt();
-        sc.nextLine();
         valor2 = sc.nextDouble();
         valorTotal = (valor1 * qtd1) + (valor2 * qtd2);
 
