0b2418d08912c6f46added517f96d7c2f899c4a6
Marina <marinaesp08@gmail.com>
2026-02-11 00:11:38 +0100

    S3: 29. Ejercicio 4 resuelto de Entrada-Procesamiento-Salida de datos

---
+++ EjResueltoEstrucSeque4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EjResueltoEstrucSeque4 {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N, resto, horas, minutos, segundos;
+
+        N = sc.nextInt();
+
+        horas = N / 3600;
+        resto = N % 3600; // el operador mod (%) devuelve el resto de una división
+        minutos = resto / 60;
+        segundos = resto % 60;
+
+        System.out.println(horas + ":" + minutos + ":" + segundos);
+
+        sc.close();
+    }
+}
