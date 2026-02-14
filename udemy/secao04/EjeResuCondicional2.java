f184404e1efe69f769fcae56458e640f4bc17fc9
Marina <marinaesp08@gmail.com>
2026-02-14 01:30:01 +0100

    S4: 36. Ejercicio 2 resuelto Estructura condicional

---
+++ udemy/secao04/EjeResuCondicional2.java
@@ -0,0 +1,29 @@
+package udemy.secao04;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EjeResuCondicional2 {
+    public static void main (String [] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+
+        double delta = B * B - 4 * A * C;
+        double r1 = (-B + Math.sqrt(delta)) / (2 * A);
+        double r2 = (-B - Math.sqrt(delta)) / (2 * A);
+
+        if (A == 0 || delta < 0.0) {
+            System.out.println("Impossivel calcular");
+        }
+        else {
+            System.out.printf("R1 = %.5f%n", r1);
+            System.out.printf("R2 = %.5f%n", r2);
+        }
+
+        sc.close();
+    }
+}
