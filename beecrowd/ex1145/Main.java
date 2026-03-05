06fba0bd014ad1e7e5991277a27271383cb456a2
Marina <marinaesp08@gmail.com>
2026-03-05 21:00:32 +0100

    S5: 56. Ejercicios en beecrowd(1145) - Secuencia lógica 2 de dos valores con for

---
+++ beecrowd/ex1145/Main.java
@@ -0,0 +1,22 @@
+package beecrowd.ex1145;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        for (int i = 1; i <= y; i++) {
+            if (i % x == 0) {
+                System.out.println(i);
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
