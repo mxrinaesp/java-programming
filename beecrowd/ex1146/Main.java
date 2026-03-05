1a8d9f2fda70a2dfdfcd00eb22b045b0ae8d5179
Marina <marinaesp08@gmail.com>
2026-03-05 21:44:07 +0100

    S5: 56. Ejercicios en beecrowd(1146) - Secuencias crecientes con while y for

---
+++ beecrowd/ex1146/Main.java
@@ -0,0 +1,25 @@
+package beecrowd.ex1146;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        while (x != 0) {
+            for (int i=1; i <= x; i++) {
+                System.out.print(i + " ");
+
+                if (i == x) {
+                    System.out.println();
+                }
+            }
+            
+            x = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
